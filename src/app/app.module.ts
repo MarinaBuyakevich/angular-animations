612d8b4540fbf9980fb41ed4174ac9d9f564ca70
MarinaBuyakevich <32779490+MarinaBuyakevich@users.noreply.github.com>
2018-12-06 16:52:52 +0200

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+  import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+  import { AppRoutingModule } from './app-routing.module';
+  import { AppComponent } from './app.component';
+  import { MatCardModule, MatButtonModule, MatToolbarModule } from '@angular/material';
+  import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+  import { PageComponent } from './page/page.component';
+  import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+  import {MatCheckboxModule} from '@angular/material';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    NoopAnimationsModule,
+    MatCheckboxModule
   ],
+  exports: [MatButtonModule, MatCheckboxModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, PageComponent]
 })
-export class AppModule { }
+export class AppModule  { }
+// export class PizzaPartyAppModule { }
+// export class MyOwnCustomMaterialModule { }
