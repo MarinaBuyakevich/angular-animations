1c464e77b1b1093153ac3d7a22bb5512cbd6464c
MarinaBuyakevich <32779490+MarinaBuyakevich@users.noreply.github.com>
2018-12-07 11:17:36 +0200

    code editing

--- src/app/shared/directives/fullpage.directive.spec.ts
+++ src/app/shared/directives/fullpage.directive.spec.ts
@@ -2,6 +2,7 @@ import { FullpageDirective } from './fullpage.directive';
 
 describe('FullpageDirective', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     const directive = new FullpageDirective();
     expect(directive).toBeTruthy();
   });
